080d6e2642a6694df8191f3e0f63a194dde94fe0
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2016-10-25 18:37:02 -0700

    Many changes

--- vim-plugin.rb
+++ vim-plugin.rb
@@ -6,23 +6,31 @@ when "install"
   puts ":: Getting repository information..."
   y = YAML.load raw_json = `curl -s https://api.github.com/repos/#{ARGV[1]}`
 
+  puts "#{y["name"]} (id #{y["id"]}) by #{y["owner"]["login"]} (id: #{y["owner"]["id"]})"
   puts "#{y["full_name"]}: #{y["description"]}"
 
-  print ":: Install #{y["name"]}? [Y/n] "
+  print ":: Install plugin #{y["name"]}? [Y/n] "
 
   exit(100) if STDIN.gets.chomp.downcase == "n"
 
-  puts "[1/2] Cloning repository #{y["full_name"]}..."
-  system "git clone https://www.github.com/#{y["full_name"]} #{Dir.home}/.vim/bundle/#{y["name"]}"
+  dir_name = "#{Dir.home}/.vim/bundle/#{y["id"]}"
 
-  puts "[2/2] Writing information..."
-  File.write(Dir.home+"/.vim/bundle/#{y["name"]}/#{y["name"]}.plugin.json", raw_json)
+  puts "(1/2) Cloning repository #{y["full_name"]}..."
+  system "git clone https://www.github.com/#{y["full_name"]} #{dir_name}"
+
+  puts "(2/2) Writing information..."
+  File.write(dir_name+"/#{y["name"]}.plugin.json", raw_json)
+
+  File.open(Dir.home+"/.vim/bundle/plugins.yml", "a") { |f|
+    f.puts "---"
+    f.puts "#{y["id"]}: #{raw_json}"
+  }
 
   puts ":: Plugin #{y["name"]} has been successfully installed."
 when "remove"
-  ARGV[1] = ARGV[1].split("/")[1]
-  raw_json = File.read Dir.home+"/.vim/bundle/#{ARGV[1]}/#{ARGV[1]}.plugin.json";
-  y = YAML.load raw_json
+  raw_yaml = File.read Dir.home+"/.vim/bundle/plugins.yml";
+  j = YAML.load raw_yaml
+  y = j[j[ARGV[1].to_i]]
 
   puts "#{y["full_name"]}: #{y["description"]}"
 
@@ -33,9 +41,18 @@ when "remove"
     exit(101)
   end
 
-  puts "[1/1] Removing files."
+  puts "(1/2) Removing files..."
 
   system "rm -rf #{Dir.home}/.vim/bundle/#{y["name"]}"
 
+  puts "(2/2) Writing information..."
+
   puts ":: Plugin #{y["name"]} has been successfully removed."
+when "list"
+  raw_json = File.read Dir.home+"/.vim/bundle/plugins.yml"
+  y = YAML.load raw_json
+  y.each do |i|
+    puts i["id"]
+  end
+
 end
